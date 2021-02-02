537cdcd9caa960468c057a0cd3e66fe2d572a7c9
Rohit Kumar Gupta <rohitgupta.dbs@gmail.com>
2021-02-03 03:22:36 +0530

    Trying Nav Click

--- js/project.js
+++ js/project.js
@@ -2,14 +2,42 @@
 window.onbeforeunload = function () {
     window.scrollTo(0, 0);
 };
+
 //Mobile Nav Screen
-$('a[href*="#"]').on("click", function (e) {
-    var $anchor = $(this);
-    $('html, body').stop().animate({
-        scrollTop: $($anchor.attr('href')).offset().top
-    }, 1500);
-    e.preventDefault(); //this is the important line.
-});
+$('a[href*="#"]')
+    // Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .not('[href="#0"]').on('click', function (event) {
+        // On-page links
+        if (
+            location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+            &&
+            location.hostname == this.hostname
+        ) {
+            // Figure out element to scroll to
+            var target = $(this.get(1).getAttribute('href'));
+            target = target.length ? target : $('[name=' + target.slice(1) + ']');
+            // Does a scroll target exist?
+            if (target.length) {
+                // Only prevent default if animation is actually gonna happen
+                event.preventDefault();
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000, function () {
+                    // Callback after animation
+                    // Must change focus!
+                    var $target = $(target);
+                    $target.focus();
+                    if ($target.is(":focus")) { // Checking if the target was focused
+                        return false;
+                    } else {
+                        $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                        $target.focus(); // Set focus again
+                    };
+                });
+            }
+        }
+    });
 $(document).on('scroll', function (e) {
     console.log('scrolling')
     if ($(this).scrollTop() + 100 < $('#About').offset().top) {
